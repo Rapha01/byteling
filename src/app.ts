837162852f70ce83d96a44ec9fd7fa58dad38448
Rapha01 <sebashatt@gmail.com>
2024-04-17 10:08:57 +0200

    feat(cd): Github actions test

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 
-  console.log(`cccc`);
+  console.log(`dddd`);
 });
 
 app.get("/", (req: Request, res: Response) => {
