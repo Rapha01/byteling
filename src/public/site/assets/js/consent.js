4ac9347721da33f29d9244051e97a071c811ae60
Rapha01 <sebashatt@gmail.com>
2024-08-21 20:23:03 +0200

    feat(cookies): consent banner and google tag manager

--- src/public/site/assets/js/consent.js
+++ src/public/site/assets/js/consent.js
@@ -1,5 +1,17 @@
 
+// Define dataLayer and the gtag function.
+/*window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
 
+// Set default consent to 'denied' as a placeholder
+// Determine actual values based on your own requirements
+console.log('setting consent default');
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',
+  'analytics_storage': 'denied'
+});*/
 
 const consentManager = {
   untickAllCheckboxes: () => {
@@ -13,6 +25,10 @@ const consentManager = {
   },
   handleConsentSubmit: () => {
     console.log('handleConsentSubmit');
+    gtag('consent', 'update', {
+      'ad_storage': 'granted'
+    });
+
   },
   showPopup: () => {
     console.log('showPopup');
